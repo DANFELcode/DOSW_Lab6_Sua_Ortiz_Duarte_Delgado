8cec47c6ba879916797315cf9a20d80635e55a90
Juan Pablo Duarte <juan.duarte-s@mail.escuelaing.edu.co>
2026-09-23 22:38:03 -0500

    feat: jerarquia de usuarios, solicitudes y notificaciones

---
+++ project/src/main/java/edu/eci/dosw/oficioya/model/Contratante.java
@@ -0,0 +1,9 @@
+package edu.eci.dosw.oficioya.model;
+
+import java.util.List;
+
+public class Contratante extends Usuario {
+
+    private String foto;
+    private List<Solicitud> solicitudes;
+}
---
+++ project/src/main/java/edu/eci/dosw/oficioya/model/Trabajador.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.oficioya.model;
+
+public class Trabajador extends Usuario {
+
+    private String foto;
+    private double tarifaAproximada;
+    private boolean disponibilidad;
+    private int trabajoCompletado;
+    private double calificacionPromedio;
+    private EstadoTrabajador estado;
+    private Oficio oficio;
+    private HistorialCartera historialCartera;
+}
